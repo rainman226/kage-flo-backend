ea6609d53cac2af623ec3db8e696a66644795d10
rainman226 <dota2romaniarobi@gmail.com>
2023-08-29 15:44:59 +0300

    removed unused classes

--- src/main/java/kageflo/repositories/AnimeGenreRepository.java
+++
@@ -1,7 +0,0 @@
-package kageflo.repositories;
-
-import kageflo.entities.AnimeGenre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimeGenreRepository extends JpaRepository<AnimeGenre, AnimeGenre.AnimeGenreID> {
-}
