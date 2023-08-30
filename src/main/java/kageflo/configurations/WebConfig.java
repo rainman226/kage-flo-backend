0c40db84f6baf71bdeb4684f07c0d78426878b90
rainman226 <dota2romaniarobi@gmail.com>
2023-08-30 16:19:25 +0300

    cleaned configs

--- src/main/java/kageflo/configurations/WebConfig.java
+++ src/main/java/kageflo/configurations/WebConfig.java
@@ -11,12 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Set the mapping URL pattern to cover all endpoints
-                .allowedOrigins("http://localhost:3000") // Set the allowed origin for CORS requests
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Set the allowed HTTP methods for CORS requests
-                .allowedHeaders("*") // Set the allowed headers for CORS requests (you can also specify specific headers)
-                .exposedHeaders("header1", "header2") // Set the exposed headers for CORS responses (optional)
-                .allowCredentials(true) // Allow sending credentials like cookies with CORS requests (optional)
-                .maxAge(3600); // Set the maximum age of the CORS pre-flight request cache (optional)
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
