db4c06048f4da9c2cc91117a5858f4496816d917
rainman226 <dota2romaniarobi@gmail.com>
2023-11-03 19:27:20 +0200

    dependency changes

---
+++ src/test/java/kageflo/repositories/AnimeRepositoryTest.java
@@ -0,0 +1,15 @@
+package kageflo.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimeRepositoryTest {
+
+    @Test
+    void searchAnimeByTitleIgnoreCase() {
+        //given
+        //when
+        //then
+    }
+}
